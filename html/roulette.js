4f9127ff1bb7b971a899b4e8da6f0bf5184ee05c
Eozost <davesprite15@gmail.com>
2024-02-14 09:58:08 -0500

    type

---
+++ html/roulette.js
@@ -0,0 +1,7 @@
+"use strict"
+
+function roulette(){
+    console.log("food")
+}
+
+roulete
--- src/Profile.py
+++ src/Profile.py
@@ -4,22 +4,26 @@ accountDetails = {
 "alice": {
     "realName": "Alice Smith",
     "dateofBirth": "Jan. 1",
-    "email": "alice@example.com"
+    "email": "alice@example.com",
+    "profpic": "/static/aliceprof.png"
 },
 "bob": {
     "realName": "Bob Jones",
     "dateofBirth": "Dec. 31",
-    "email": "bob@bob.xyz"
+    "email": "bob@bob.xyz",
+    "profpic": "/static/bobprof.png"
 },
 "carol": {
     "realName": "Carol Ling",
     "dateofBirth": "Jul. 17",
-    "email": "carol@example.com"
+    "email": "carol@example.com",
+    "profpic": "/static/carol.png"
 },
 "dave": {
     "realName": "Dave N. Port",
     "dateofBirth": "Mar. 14",
-    "email": "dave@dave.dave"
+    "email": "dave@dave.dave",
+    "profpic": "/static/simplesketch.png"
 }
 }
 class profileHandler(tornado.web.RequestHandler):
@@ -28,12 +32,14 @@ class profileHandler(tornado.web.RequestHandler):
             rname = accountDetails[user]["realName"]
             dob = accountDetails[user]["dateofBirth"]
             email = accountDetails[user]["email"]
+            profilepic = accountDetails[user]["profpic"]
 
         
             self.render("TemplateTest.html",
                     Realname=rname,
                     dateofBirth=dob,
                     Adress = email,
-                    uname = user
+                    uname = user,
+                    image = profilepic
             )       
     
