7dfc7902e44ab49f40167d6a523c07fd4a19929a
Eozost <davesprite15@gmail.com>
2024-04-10 09:15:52 -0400

    Changed the javascript stuff

--- html/roulette.js
+++ html/roulette.js
@@ -1,4 +1,3 @@
-// Define the roulette wheel
 var wheel = ["00"].concat(Array.from({length: 36}, (_, i) => i.toString()));
 
 function spinRoulette() {
@@ -6,13 +5,34 @@ function spinRoulette() {
     var spin = wheel[Math.floor(Math.random() * wheel.length)];
     
     // Determine color
-    var color = ["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"].includes(spin) ? "Rouge" : "Noir";
+    var color;
+    if (spin === "0") {
+        color = "Rouge";
+    } else if (spin === "00") {
+        color = "Noir";
+    } else {
+        color = ["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"].includes(spin) ? "Rouge" : "Noir";
+    }
     
     // Determine even/odd
-    var evenOdd = spin === "0" || spin === "00" ? "Neither Pair nor Impair" : parseInt(spin) % 2 === 0 ? "Pair" : "Impair";
+    var evenOdd;
+    if (spin === "0") {
+        evenOdd = "Impair";
+    } else if (spin === "00") {
+        evenOdd = "Pair";
+    } else {
+        evenOdd = parseInt(spin) % 2 === 0 ? "Pair" : "Impair";
+    }
     
     // Determine failed/passed
-    var failedPassed = spin === "0" || spin === "00" ? "Neither Manque nor Passe" : parseInt(spin) <= 18 ? "Manque" : "Passe";
+    var failedPassed;
+    if (spin === "0") {
+        failedPassed = "Manque";
+    } else if (spin === "00") {
+        failedPassed = "Passe";
+    } else {
+        failedPassed = parseInt(spin) <= 18 ? "Manque" : "Passe";
+    }
     
     // Create a results row
     var resultsRow = "<tr>" +
