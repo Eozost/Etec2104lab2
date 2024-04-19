a97a132d3ab13dac987dcb88b15a7a5af32935c0
Eozost <davesprite15@gmail.com>
2024-04-18 22:04:04 -0400

    Commit message

--- src/roulette.py
+++ src/roulette.py
@@ -1,29 +1,5 @@
-import tornado.websocket
-import tornado.ioloop
 import tornado.web
-import json
 
-activeClients = []
-
-class Handler(tornado.websocket.WebSocketHandler):
-    async def open(self):
-        activeClients.append(self)
-
-    async def on_message(self, msg):
-        data = json.loads(msg)
-        for c in activeClients:
-            await c.write_message(json.dumps(data))
-
-    def on_close(self):
-        activeClients.remove(self)
-
-    def check_origin(self, *args):
-        return True
-def make_app():
-    return tornado.web.Application([
-        (r"/roulette", Handler),
-    ])
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8000)
-    tornado.ioloop.IOLoop.current().start()
+class Handler(tornado.web.RequestHandler):
+    def get(self):
+        self.render( "../html/roulette.html")
